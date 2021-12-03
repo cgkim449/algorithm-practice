6429fead68124b3cddd8d449461a7ffa466e99f6
Kim Changu <cgkim449@gmail.com>
2021-12-03 21:11:06 +0900

    Create C4A.java

---
+++ src/codeforces/C4A.java
@@ -0,0 +1,22 @@
+package codeforces;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+/**
+ * Created by cgkim449
+ * Date: 2021-12-03
+ * Time: 21:00
+ * https://codeforces.com/problemset/problem/4/A
+ */
+public class C4A {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(br.readLine());
+        System.out.println(n % 2 == 0 && n > 2? "YES" : "NO");
+
+        br.close();
+    }
+}
