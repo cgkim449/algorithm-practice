3420fb723fb543c674c2866f172926b023f65c98
Kim Changu <cgkim449@gmail.com>
2021-12-06 23:33:53 +0900

    Create C50A.java

---
+++ src/codeforces/C50A.java
@@ -0,0 +1,14 @@
+package codeforces;
+
+import java.util.Scanner;
+
+public class C50A {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int m = sc.nextInt();
+        sc.nextLine();
+        System.out.println(n*m/2);
+        sc.close();
+    }
+}
