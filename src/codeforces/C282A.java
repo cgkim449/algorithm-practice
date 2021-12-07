4652c5de135743252509588cecbdb22671984db3
Kim Changu <cgkim449@gmail.com>
2021-12-07 23:36:21 +0900

    Create C282A.java

---
+++ src/codeforces/C282A.java
@@ -0,0 +1,25 @@
+package codeforces;
+
+import java.util.Scanner;
+
+/**
+ * Date: 2021-12-07
+ * Time: 23:27
+ * https://codeforces.com/problemset/problem/282/A
+ */
+public class C282A {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = Integer.parseInt(sc.nextLine());
+        int answer = 0;
+        while(n-- > 0) {
+            if(sc.nextLine().indexOf('+') != -1) {
+              answer++;
+            } else {
+                answer--;
+            }
+        }
+        System.out.println(answer);
+        sc.close();
+    }
+}
