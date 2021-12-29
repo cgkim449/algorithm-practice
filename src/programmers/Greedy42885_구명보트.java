cb3fe6cc3f728e2ce820634e8bb12d85b645d86a
Kim Changu <cgkim449@gmail.com>
2021-12-29 12:05:20 +0900

    Create Greedy42885_구명보트.java

---
+++ src/programmers/Greedy42885_구명보트.java
@@ -0,0 +1,42 @@
+package programmers;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Deque;
+import java.util.LinkedList;
+
+/**
+ * Date: 2021-12-29
+ * Time: 09:21
+ * https://programmers.co.kr/learn/courses/30/lessons/42885
+ */
+public class Greedy42885_구명보트 {
+    public static void main(String[] args) {
+        int[] people = new int[]{70, 80, 50};
+        int limit = 100;
+        System.out.println(solution(people, limit));
+    }
+    public static int solution(int[] people, int limit) {
+        ArrayList<Integer> arrayList = new ArrayList<>();
+        for (int person : people) {
+            arrayList.add(person);
+        }
+
+        Collections.sort(arrayList);
+        Deque<Integer> deque = new LinkedList<>(arrayList);
+
+        int count = 0;
+
+        while (true) {
+            if(deque.isEmpty()) return count;
+            
+            int x = deque.pollLast();
+            count++;
+
+            if(deque.isEmpty())  return count;
+
+            int y = deque.peekFirst();
+            if(limit >= x + y) deque.pollFirst();
+        }
+    }
+}
